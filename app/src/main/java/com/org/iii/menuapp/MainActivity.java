c4b0f742c20a7c97df9bc4639871746b0b3775bd
W10User <leo0908w@yahoo.com.tw>
2016-12-18 23:13:04 +0800

    v2

--- app/src/main/java/com/org/iii/menuapp/MainActivity.java
+++ app/src/main/java/com/org/iii/menuapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.org.iii.menuapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.widget.ExpandableListView;
 import android.widget.SimpleExpandableListAdapter;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,10 +20,13 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     private String[] menu_group = {"麵", "飯", "湯"};
     private String[][] menu_sub = {{ "炒麵", "義大利麵" }, { "炒飯", "咖哩飯", "滷肉飯" }, {"菜頭湯", "葛利湯"}};
-    private List<Map<String, String>> group = new ArrayList<Map<String, String>>();
-    private List<List<Map<String, String>>> childrenList = new ArrayList<List<Map<String, String>>>();
+    private int[][] price = {{1, 2}, {3, 4, 5}, {6, 7}};
+    private int[] img = {R.drawable.dish0, R.drawable.dish1, R.drawable.dish2};
+    private List<Map<String, String>> group = new ArrayList<>();
+    private List<List<Map<String, String>>> childrenList = new ArrayList<>();
     private ExpandableListView expListView;
     private Context context;
+    private Bitmap bitmap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,14 +39,14 @@ public class MainActivity extends AppCompatActivity {
         // 將 menu_group & menu_sub 資料拆解並配置到 group & childrenList
         for (int i = 0; i< menu_group.length ; i++ ) {
             // Group
-            Map<String, String> groupFood = new HashMap<String, String>();
+            Map<String, String> groupFood = new HashMap<>();
             groupFood.put("groupKey", menu_group[i]);
             group.add(groupFood);
 
             // Sub
-            List<Map<String, String>> children = new ArrayList<Map<String, String>>();
+            List<Map<String, String>> children = new ArrayList<>();
             for (String food : menu_sub[i]) {
-                Map<String, String> childMap = new HashMap<String, String>();
+                Map<String, String> childMap = new HashMap<>();
                 childMap.put("childKey", food);
                 children.add(childMap);
             }
@@ -75,13 +82,30 @@ public class MainActivity extends AppCompatActivity {
                 String subfood = menu_sub[groupPosition][childPosition];
                 Toast.makeText(context, mainfood + ":" + subfood,
                         Toast.LENGTH_SHORT).show();
+                bitmap = BitmapFactory.decodeResource(getResources(), img[2]);
+//                int test = bitmap.getByteCount();
+//                Log.v("will", "byteSize"+test);
+
+                ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,bs);
+
+                Intent it = new Intent(MainActivity.this, Main2Activity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("meal", subfood);
+//                it.putExtra("meal", subfood);
+                bundle.putInt("price", price[0][0]);
+//                it.putExtra("price", price[0][1]);
+                bundle.putByteArray("img", bs.toByteArray());
+//                it.putExtra("img", bs.toByteArray());
+                it.putExtras(bundle);
+                startActivity(it);
+
                 return false;
             }
 
         });
 
     }
-
 
 
 }
