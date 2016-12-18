0df6ffb3a6e0b47fa9490184c4f9f0ab50217d34
W10User <leo0908w@yahoo.com.tw>
2016-12-18 23:15:10 +0800

    v2

---
+++ app/src/main/java/com/org/iii/menuapp/Main2Activity.java
@@ -0,0 +1,53 @@
+package com.org.iii.menuapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    private TextView mealText;
+    private TextView priceText;
+    private ImageView imageView;
+    private Intent it;
+    private int[] img = {R.drawable.dish0, R.drawable.dish1, R.drawable.dish2};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mealText = (TextView) findViewById(R.id.meal);
+        priceText = (TextView) findViewById(R.id.price);
+        imageView = (ImageView) findViewById(R.id.img);
+
+        Bundle bundle = getIntent().getExtras();
+//        it = getIntent();
+        String meal = bundle.getString("meal");
+//        String meal = it.getStringExtra("meal");
+        int price = bundle.getInt("price", -1);
+//        int price = it.getIntExtra("price", -1);
+        byte[] b = bundle.getByteArray("img");
+//        byte[] b = it.getByteArrayExtra("img");
+        Bitmap bmp = BitmapFactory.decodeByteArray(b, 0, b.length);
+
+
+        Log.v("will", meal + "::" +price);
+//        Log.v("will", "img" + bitmap);
+
+        mealText.setText(meal);
+        priceText.setText(String.valueOf(price));
+        imageView.setImageBitmap(bmp);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
+
+
